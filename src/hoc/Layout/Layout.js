817b9af330c8ee818a71fe0c49447f29460e78d9
susiesko <susie.sko@gmail.com>
2021-01-06 19:01:06 -0800

    trying to figure out app layout

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -6,11 +6,16 @@ import Header from '../../containers/Header/Header';
 import Content from '../../containers/Content/Content';
 import Footer from '../../containers/Footer/Footer';
 
+import classes from './Layout.module.css';
+
 export class Layout extends Component {
   render() {
     return (
       <Aux>
         <Header/>
+        <main className={classes.classes}>
+          
+        </main>
         <Content/>
         <Footer/>
       </Aux>
